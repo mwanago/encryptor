85a497ca7f02cc7c3195584334dcec7b02e4eb48
mwanago <wanago.marcin@gmail.com>
2019-09-01 19:49:29 +0200

    chore(): add scss support

---
+++ src/components/App/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import FilePicker from "../FilePicker/FilePicker";
+import styles from './styles.scss';
+
+const App: React.FC = () => {
+  return (
+    <div className={styles.wrapper}>
+      <FilePicker />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/FilePicker/FilePicker.tsx
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class FilePicker extends Component {
+    render() {
+        return (
+            <div>
+                File picker
+            </div>
+        )
+    }
+}
+
+export default FilePicker;
